95f68ed7eb4678b343afd082cda033746b62f91e
camyoung54 <camyoung54@gmail.com>
2023-05-03 16:46:39 +0100

    edit model function

--- R/models.R
+++ R/models.R
@@ -2,7 +2,10 @@
 
 
 
-CopyClust_10_Class_Scale_Model_Prediction = function() {
-   as.data.frame(predict(CopyClust_10_Class_Scale_Function_v2, TCGA_data))
+CopyClust_10_Class_Scale_Model_Prediction = function(data_input) {
+   prediction = as.data.frame(predict(CopyClust_10_Class_Scale_Function_v2, data_input))
+   return(prediction)
 }
+
+
 
