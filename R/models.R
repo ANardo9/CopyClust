d20235cefaf0c2bb6d1819607c37311ee1a22c63
camyoung54 <camyoung54@gmail.com>
2023-05-03 17:52:14 +0100

    add documentation

--- R/models.R
+++ R/models.R
@@ -1,7 +1,9 @@
-#CopyClust
+#' CopyClust Model Prediction
+#'
+#' @param data_input A data frame with sample IDs as rows and 478 model features as columns.
+#' @returns A numeric vector of predicted Integrative Cluster label.
 
-
-CopyClust_10_Class_Scale_Model_Prediction = function(data_input) {
+CopyClust_Model_Prediction = function(data_input) {
    prediction = as.data.frame(predict(CopyClust_10_Class_Scale_Function_v2, data_input)) + 1
    rownames(prediction) = rownames(data_input)
    colnames(prediction) = "IntClust Label"
