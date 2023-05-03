1e2f19c4a45ce501fd9e9dc5901ceb89083a2c68
camyoung54 <camyoung54@gmail.com>
2023-05-03 17:05:44 +0100

    edit data function

---
+++ R/data.R
@@ -0,0 +1,22 @@
+#' CopyClust 10 Class Model Coefficients
+#'
+#' 10-Class model coefficients determined from XGBoost
+#'
+#' @format An xgb.Booster class list object with 3 variables: \code{handle}, \code{raw}, \code{niter}
+"CopyClust_10_Class_Scale_Function_v2"
+
+#' CopyClust Example Data from TCGA
+#'
+#' Example data from TCGA samples used to test the package
+#'
+#' @format A 1075 by 478 matrix
+"tcga_test_data"
+
+
+#' CopyClust Example TCGA IntClust Labels
+#'
+#' IntClust labels of example TCGA samples
+#'
+#' @format A 1075 by 2 matrix
+"tcga_test_labels"
+
--- R/models.R
+++ R/models.R
@@ -1,9 +1,11 @@
-
-
+#CopyClust
 
 
 CopyClust_10_Class_Scale_Model_Prediction = function(data_input) {
-   prediction = as.data.frame(predict(CopyClust_10_Class_Scale_Function_v2, data_input))
+   prediction = as.data.frame(predict(CopyClust_10_Class_Scale_Function_v2, data_input)) + 1
+   rownames(prediction) = rownames(data_input)
+   colnames(prediction) = "IntClust Label"
+
    return(prediction)
 }
 
