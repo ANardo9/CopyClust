5b0fdaf6662b1def8aaa87936021a903e5575787
camyoung54 <camyoung54@gmail.com>
2023-05-03 21:19:50 +0100

    change function name

--- R/models.R
+++ R/models.R
@@ -4,7 +4,7 @@
 #' @param model_approach If TRUE, implement 10-class model approach. If FALSE, implement 6-class model approach with binary reclassification.
 #' @returns A numeric vector of predicted Integrative Cluster label according to model approach.
 
-CopyClust_Model_Prediction = function(data_input, model_approach = TRUE) {
+CopyClust = function(data_input, model_approach = TRUE) {
   if (model_approach == TRUE) {
 
    prediction = as.data.frame(predict(CopyClust_10_Class_Scale_Function_v2, data_input)) + 1
@@ -14,9 +14,19 @@ CopyClust_Model_Prediction = function(data_input, model_approach = TRUE) {
    return(prediction)
   }
   else {
-    print("6-Class Model Approach")
-  }
+    prediction = as.data.frame(predict(CopyClust_6_Class_Scale_Function_v2, data_input)) + 1
+    rownames(prediction) = rownames(data_input)
+    colnames(prediction) = "IntClust Label"
+
+    return(prediction)
+    }
 }
+
+
+
+
+
+
 
 
 
