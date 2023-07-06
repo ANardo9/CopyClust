4dd27675b4a3e65634205a4cb91e52a57686b878
Cameron Young <cameronyoung@Camerons-MBP-6.fios-router.home>
2023-07-06 13:58:51 -0400

    change parameter of copyclust function

--- R/models.R
+++ R/models.R
@@ -4,8 +4,8 @@
 #' @param model_approach If TRUE, implement 10-class model approach. If FALSE, implement 6-class model approach with binary reclassification.
 #' @returns A numeric vector of predicted Integrative Cluster label according to model approach.
 
-CopyClust = function(data_input, model_approach = TRUE) {
-  if (model_approach == TRUE) {
+CopyClust = function(data_input, model_approach = "10C") {
+  if (model_approach == "10C") {
 
    prediction = as.data.frame(predict(CopyClust_10_Class_Scale_Function_v2, data_input)) + 1
    rownames(prediction) = rownames(data_input)
